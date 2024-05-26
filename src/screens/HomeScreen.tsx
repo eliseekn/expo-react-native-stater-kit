80eb95d717fc985579ff7d71ae13c39503956757
eliseekn <eliseekn@gmail.com>
2024-05-26 11:09:27 +0000

    Refactor code Update package dependencies

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Text} from 'react-native-paper'
 import {SafeAreaView} from 'react-native'
+import style from '../style'
 
 const HomeScreen: React.FC = () => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={style.container}>
             <Text>Hello 👋</Text>
         </SafeAreaView>
     )
---
+++ src/style/index.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native'
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+    },
+})
+
+export default style
